bc346434262d82967fd73f3771b61963c31a09ca
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-07-27 08:35:57 -0700

    second get by id

--- server.js
+++ server.js
@@ -11,8 +11,24 @@ server.use(express.json());
 
 server.listen(port, () => console.log(`Server is listening port ${port}`));
 
+server.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1>');
+})
+
 server.get('/projects', (req, res) => {
     projects.get().then(p => {
+        res.status(200).json(p)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'The projects information could not be retrieved' })
+    })
+})
+server.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    projects.get(id).then(p => {
+        if(p.length === 0){ // <--- need to fix
+            res.status(404).json({ error: 'The project with specified ID does not exist' })
+        }
         res.status(200).json(p)
     })
     .catch(err => {
